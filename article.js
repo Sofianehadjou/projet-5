43411af66ef22cacfb7a2d4534e1afa0013f27e7
sofiane hadjou <sofianehadjou1993@gmail.com>
2020-05-24 03:35:37 +0200

    affichage de tous les articles sur la page d'accueil

--- article.js
+++ article.js
@@ -1,22 +1,7 @@
-//appeler l'élement produit:
-let productContent = document.getElementById('productInfos');
-let productId =document.querySelector('#productId p')
-//appeler le titre du produit:
-let productName = document.querySelector('#productName h2')
-//appeler la div prix: 
-let productPrice = document.querySelector('#productPrice  p');
-//appeler la div description:
-let productDescription = document.querySelector('#productDescription p');
-//Appeler la div Lenses:
-let productLenses = document.querySelector('#productLenses p');
-//créer la div IMG:
-var productImages = document.getElementById('productImages')
 //créer la variable pour l' Url d'api
-var requestURL = 'http://localhost:3000/api/cameras';
+let requestURL = 'http://localhost:3000/api/cameras';
 
-
-
-let products = document.getElementById('products');
+var products = document.getElementById('products');
 
 
 //recuperer les données de l'API:
@@ -24,14 +9,17 @@ var request = new XMLHttpRequest();   //Lig on crée un nouvel objet qui corresp
 request.onreadystatechange = function() {
     if (this.readyState == XMLHttpRequest.DONE && this.status == 200) {
         var response =JSON.parse(this.responseText);
-       for( let i = 0; i < response.length; i++){
-           let article = response[i];
+       for( let i = 0; i < response.length; i++) {
+           let article = response[i]; 
            
-// Ajouter le contenu au DOM
-           products.innerHTML = `<article>
+// Ajouter le contenu au DOM:
+ 
+
+console.log(article);
+           products.innerHTML += `<article>
                 <div id="productInfos">
-                    <div class="bloc-produit">
-                        <p id="productId"></p>
+                    <div id="bloc-produit">
+                        <p class="productId"></p>
                         <h2 id="productName"><strong>Nom: </strong>${response[i].name}</h2>
                         <p id="productPrice"><strong>Prix: </strong> ${response[i].price/100} €</p>
                         <p id="productDescription"><strong>Description: </strong> ${response[i].description}</p>
@@ -42,13 +30,11 @@ request.onreadystatechange = function() {
                     </div>
                 </div>
                 <a class="btn btn-shopping" href="produit.html?id=${response[i]._id}" aria-label="Sélectionner un article ${response[i].name}">Sélectionner</a>
-            </article>`;      
+            </article>
+            `;
        }
-    }
+    } 
 }
 
 request.open("GET", requestURL);
 request.send();
-
-
-
