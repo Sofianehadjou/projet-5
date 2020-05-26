3e667635b7d886068d4d69b8d4a47e1ba67f3785
sofiane hadjou <sofianehadjou1993@gmail.com>
2020-05-26 17:21:38 +0200

    ajout des pages produit et panier

--- article.js
+++ article.js
@@ -3,6 +3,8 @@ let requestURL = 'http://localhost:3000/api/cameras';
 
 var products = document.getElementById('products');
 
+var productUnit= document.getElementById('productUnit');
+
 
 //recuperer les données de l'API:
 var request = new XMLHttpRequest();   //Lig on crée un nouvel objet qui correspond à notre objet AJAX. C'est grâce à lui qu'on va créer et envoyer notre requête ;
@@ -13,28 +15,26 @@ request.onreadystatechange = function() {
            let article = response[i]; 
            
 // Ajouter le contenu au DOM:
- 
-
-console.log(article);
-           products.innerHTML += `<article>
+            products.innerHTML += `<article class="article">
                 <div id="productInfos">
                     <div id="bloc-produit">
                         <p class="productId"></p>
-                        <h2 id="productName"><strong>Nom: </strong>${response[i].name}</h2>
-                        <p id="productPrice"><strong>Prix: </strong> ${response[i].price/100} €</p>
-                        <p id="productDescription"><strong>Description: </strong> ${response[i].description}</p>
-                        <p id="productLenses"><strong>Lenses: </strong> ${response[i].lenses}</p>
+                        <h2 class="productName"><strong>Nom: </strong>${response[i].name}</h2>
+                        <p class="productPrice"><strong>Prix: </strong> ${response[i].price/100} €</p>
+                        <p class="productDescription"><strong>Description: </strong> ${response[i].description}</p>
+                        <p classd="productLenses"><strong>Lenses: </strong> ${response[i].lenses}</p>
                     </div>
                     <div> 
                         <img id="ProductImg" src="${response[i].imageUrl}" alt="">
                     </div>
                 </div>
-                <a class="btn btn-shopping" href="produit.html?id=${response[i]._id}" aria-label="Sélectionner un article ${response[i].name}">Sélectionner</a>
-            </article>
-            `;
-       }
-    } 
-}
+                <a class="btn btn-shopping" href="produit.html?id=${response[i]._id}" aria-label="">Sélectionner</a>
+            </article>`;
+       };
+    };
+};
 
-request.open("GET", requestURL);
-request.send();
+//Création et configuration d'une requête HTTP
+request.open("GET", requestURL);  
+// Envoi de la requête en y incluant l'objet
+request.send(); 
